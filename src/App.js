2baffad36e51aa76a135117eab8a0c7dfa7783ed
Cristian <cristian@digio.es>
2018-07-27 19:31:12 +0200

    Add parallax background

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App parallax">
+        <div id="stars1"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+        <div id="title">
+            <span>HI, <br/> I AM CRISTIAN BOTELLA HURTADO</span>
+        </div>
       </div>
     )
   }
