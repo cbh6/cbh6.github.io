79db9a44d8d48c0f38e06ca65dcbf5ffe748d5da
Cristian Botella <cri.bh6@gmail.com>
2018-08-04 12:57:07 +0200

    Testing ssh 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react' 
 // import ParallaxBackground from './components/ParallaxBackground'
 import StarsBackground from './components/StarsBackground';
 class App extends Component {
