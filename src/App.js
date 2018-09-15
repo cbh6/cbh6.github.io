496a9b374ac5f1b047a4c0b38716b59d4efdf983
Cristian Botella <cri.bh6@gmail.com>
2018-09-15 18:58:57 +0200

    Fix intro section styles and add posts section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import StarsBackground from './components/StarsBackground'
 import LandingPage from './pages/LandingPage'
+import PostsPage from './pages/PostsPage'
 class App extends Component {
   render() {
     return (
       <div>
         <StarsBackground />
         <LandingPage />
+        <PostsPage />
       </div>
     )
   }
--- src/components/SocialLinks/SocialLinks.js
+++ src/components/SocialLinks/SocialLinks.js
@@ -1,38 +1,51 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faTwitter, faMedium, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import {
+  faGithub,
+  faTwitter,
+  faMedium,
+  faLinkedin,
+  faStackOverflow
+} from '@fortawesome/free-brands-svg-icons'
 import './style.css'
 
 const SocialLinks = () => {
   return (
-    <div className="social-icons animate-icons">
+    <div className="social-icons">
       <a
         target="_blank"
         rel="noopener noreferrer"
         href="https://github.com/cbh6"
       >
-        <FontAwesomeIcon className="social-icons animate-icons" icon={faGithub} />
+        <FontAwesomeIcon icon={faGithub} />
       </a>
       <a
         target="_blank"
         rel="noopener noreferrer"
         href="https://twitter.com/cribh6"
       >
-        <FontAwesomeIcon className="social-icons animate-icons" icon={faTwitter} />
+        <FontAwesomeIcon icon={faTwitter} />
       </a>
       <a
         target="_blank"
         rel="noopener noreferrer"
         href="https://www.linkedin.com/in/cristian-botella-hurtado-5aba55140/"
       >
-        <FontAwesomeIcon className="social-icons animate-icons" icon={faLinkedin} />
+        <FontAwesomeIcon icon={faLinkedin} />
       </a>
       <a
         target="_blank"
         rel="noopener noreferrer"
         href="https://medium.com/@cri.bh6"
       >
-        <FontAwesomeIcon className="social-icons animate-icons" icon={faMedium} />
+        <FontAwesomeIcon icon={faMedium} />
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://stackoverflow.com/users/8938708/cbh6"
+      >
+        <FontAwesomeIcon icon={faStackOverflow} />
       </a>
     </div>
   )
